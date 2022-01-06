93d7a6b6f4a040d2624b2a70790bcfc18289e027
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-06 17:45:16 +0100

    chunk
    -write server test
    - update all folders

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const app = require('../app');
+
+
+chai.use(chaiHttp);
+chai.should()
+
+const {
+  expect
+} = chai;
+
+describe('Server Test', () => {
+  it('it should connect to server', (done) => {
+    chai.request(app)
+      .get('/home')
+      .end((error, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
