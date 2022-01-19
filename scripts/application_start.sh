915b2ca32cede723a69c09810083969016949efc
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-19 21:22:16 +0100

    fix sript

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,13 +7,13 @@ sudo chmod -R 777 /home/ec2-user/feather-app
 cd /home/ec2-user/feather-app
 
 #add npm and node to path
-HOME="/home/ec2-user"
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# HOME="/home/ec2-user"
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
 
 #start our node app in the background
-node server.js > app.out.log 2> app.err.log < /dev/null &
+npm run start
