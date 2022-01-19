187435b40fbefedfdda231e45a31608b0b6a863a
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-19 18:10:25 +0100

    fix script

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 #Stopping existing
-cd /home/ec2-user/feather-app
+
 echo "Stopping existing node servers"
-pkill node
+cd /home/ec2-user/feather-app && pkill node
