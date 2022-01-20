4674d59eb1e497cbf4ad65ecc4e440b60a4bf5c3
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-20 14:00:55 +0100

    fix sms

--- subscribers/sms.js
+++ subscribers/sms.js
@@ -9,12 +9,12 @@ const twilio = require('twilio')(config.twilio_sid, config.twilio_auth_token)
 eventEmitter.on('signup', async (data) => {
     //send sms
     const message = `Dear ${data.fullName}, to complete your registration on the feather app, you are required to use this code: ${data.code} to complete your registration. DO NOT DISCLOSE TO ANYONE`;
-    
+    const phone = data.phoneNumber.length == 11 ? "+234" + data.phoneNumber.substring(1) : data.phoneNumber;
     twilio.messages
     .create({
        body: message,
        from: config.twilio_sender_number,
-       to: data.phoneNumber
+       to: phone
      })
     .then(message => {
         logger.info(message.sid);
