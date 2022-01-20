b771d306834a8d62e376da0aec2a103884cf0fcc
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-20 15:25:31 +0100

    complete app_Start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,11 +6,10 @@ sudo chmod -R 777 /home/ec2-user/feather-app
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/feather-app
 
-#add npm and node to path
-# HOME="/home/ec2-user"
-# export NVM_DIR="$HOME/.nvm"	
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
