9e17dad3f555199d6497c5526dad35a5e57f1bb8
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-01-25 19:31:40 +0100

    feat
    funding feature

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -1,10 +1,8 @@
-const services = require("../../services").services
 exports.home = ( async (req, res) => {
     // let banks = await services.listBanks()
-    let banks = await services.initializeTransaction()
     return res.status(200).json({
         status : true,
-        data: banks,
+        data: {},
         message: "Working Successfully"
     })
 })
