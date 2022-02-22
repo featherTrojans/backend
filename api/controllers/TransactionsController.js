088b4eb550e925c4a19a8031ffd17c60bea8ccf6
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-02-22 12:03:45 +0100

    feat
    create transactions feat

---
+++ api/controllers/TransactionsController.js
@@ -0,0 +1,34 @@
+const { config } = require("../../config");
+const { Transactions } = require("../../models");
+const logger = config.logger
+
+exports.transactions = ( async (req, res) => {
+
+    const { userId } = req.user
+    try
+    {
+        const transactions = await Transactions.findAll({
+            attributes: ['transId', 'initialBal', 'amount', 'finalBal', 'description', 'from', 'to', 'direction', 'createdAt'],
+            where: {userUid: userId},
+            order: [['createdAt', 'DESC']],
+        })
+        return res.status(200).json({
+            status: true,
+            data : {
+
+                transactions
+
+            },
+            message: "success"
+        })
+
+
+    } catch (error) {
+        logger.info(error)
+        return res.status(409).json({
+            status: false,
+            data : error,
+            message: "error occur"
+        })
+    }
+});
