dd8c0f725db7648c94c9a6d19f8987c4911aff2b
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2022-09-08 23:00:13 +0100

    hot fix

--- services/treatStatuses.js
+++ services/treatStatuses.js
@@ -32,7 +32,7 @@ const treatStatuses = async (yesterday = timeService.serverTime().yesterday) => 
 // ...
 
 // // Schedule tasks to be run on the server.
-cron.schedule('30 * * * *', function() {
+cron.schedule('0 * * * *', function() {
     treatStatuses()
 });
 
