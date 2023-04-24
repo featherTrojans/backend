5f835e78f0bf3a73638e06dbe703838d9a4427d5
Ezekiel Adejobi <ezekieladejobi@Ezekiels-MacBook-Pro-2.local>
2023-04-24 21:51:09 +0100

    add sms otp to forgot pasword

--- subscribers/sms.js
+++ subscribers/sms.js
@@ -7,8 +7,22 @@ const sendSMS = require('../services/middlewares/termiiService').sendSMS
 const twilio = require('twilio')(config.twilio_sid, config.twilio_auth_token)
 
 
-
 eventEmitter.on('signup', async (data) => {
+    //send sms
+    const message = data.message;
+    // const phone = data.phoneNumber.length == 11 ? "+234" + data.phoneNumber.substring(1) : "+" + data.phoneNumber;
+    const phone = data.phoneNumber.length == 11 ? "234" + data.phoneNumber.substring(1) : data.phoneNumber;
+    try {
+        sendSMS({to: phone, message, type: "OTP"})
+
+    } catch (error) {
+        logger.info(error)
+    }
+
+
+})
+
+eventEmitter.on('changePassword', async (data) => {
     //send sms
     const message = data.message;
     // const phone = data.phoneNumber.length == 11 ? "+234" + data.phoneNumber.substring(1) : "+" + data.phoneNumber;
