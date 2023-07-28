47488976d28916c86983206bffd3ce18608f8d53
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2023-07-28 07:31:49 +0100

    fixe bugs

--- config/database.js
+++ config/database.js
@@ -5,11 +5,12 @@ const { config } = require('./')
 exports.connection = config.environment == 'live' ? new Sequelize(config.db_name, config.db_username, config.db_password, {
     dialect: 'mysql',
     host: config.db_host,
-    dialectOptions: {
-        ssl: {
-            ca: readFileSync('../../../etc/ssl/certs/ca-certificate.crt')
-        }
-    },
+    port: 25060,
+    // dialectOptions: {
+    //     ssl: {
+    //         ca: readFileSync('../../../etc/ssl/certs/ca-certificate.crt')
+    //     }
+    // },
     logging: (msg) => {
         appendFileSync("db.out.log", msg + "\n \n", "UTF-8",{'flags': 'a+'});
     }
